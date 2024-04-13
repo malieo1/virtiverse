0bc529407b9408ffbc2c1ca887a750fee9830196
rezguiamine <mohammedamine.rezgui@etudiant-fst.utm.tn>
2024-04-13 19:15:22 +0100

    springboot security

---
+++ src/main/java/com/example/virtiverse/VirtiverseApplication.java
@@ -0,0 +1,13 @@
+package com.example.virtiverse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtiverseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VirtiverseApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/virtiverse/serviceImp/AuthService.java
@@ -0,0 +1,100 @@
+package com.example.virtiverse.serviceImp;
+
+import com.example.virtiverse.dto.ReqRes;
+import com.example.virtiverse.entities.User;
+import com.example.virtiverse.repository.OurUserRepo;
+import com.example.virtiverse.util.JWTUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+
+import java.util.HashMap;
+
+@Service
+public class AuthService {
+    @Autowired
+    private OurUserRepo ourUserRepo;
+    @Autowired
+    private JWTUtils jwtUtils;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    public ReqRes signUp(ReqRes registrationRequest) {
+        ReqRes resp = new ReqRes();
+        try {
+            String emailRegex = "^[A-Za-z0-9+_.-]+@esprit\\.tn$";
+            String passwordRegex = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$";
+
+            // Validate email format
+            if (!registrationRequest.getEmail().matches(emailRegex)) {
+                resp.setStatusCode(400); // Bad request
+                resp.setMessage("Invalid email format. Email must end with @esprit.tn");
+                return resp;
+            }
+
+            // Validate password format
+            if (!registrationRequest.getPassword().matches(passwordRegex)) {
+                resp.setStatusCode(400); // Bad request
+                resp.setMessage("Invalid password format. Password must contain at least 8 characters, including letters and numbers.");
+                return resp;
+            }
+
+            User ourUsers = new User();
+            ourUsers.setEmail(registrationRequest.getEmail());
+            ourUsers.setPassword(passwordEncoder.encode(registrationRequest.getPassword()));
+            ourUsers.setRole(registrationRequest.getRole());
+            User ourUserResult = ourUserRepo.save(ourUsers);
+
+            if (ourUserResult != null && ourUserResult.getId() > 0) {
+                resp.setOurUsers(ourUserResult);
+                resp.setMessage("User Saved Successfully");
+                resp.setStatusCode(200);
+            }
+        } catch (Exception e) {
+            resp.setStatusCode(500);
+            resp.setError(e.getMessage());
+        }
+        return resp;
+    }
+
+    public ReqRes signIn(ReqRes signinRequest){
+        ReqRes response = new ReqRes();
+
+        try {
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(signinRequest.getEmail(),signinRequest.getPassword()));
+            var user = ourUserRepo.findByEmail(signinRequest.getEmail()).orElseThrow();
+            System.out.println("USER IS: "+ user);
+            var jwt = jwtUtils.generateToken(user);
+            var refreshToken = jwtUtils.generateRefreshToken(new HashMap<>(), user);
+            response.setStatusCode(200);
+            response.setToken(jwt);
+            response.setRefreshToken(refreshToken);
+            response.setExpirationTime("24Hr");
+            response.setMessage("Successfully Signed In");
+        }catch (Exception e){
+            response.setStatusCode(500);
+            response.setError(e.getMessage());
+        }
+        return response;
+    }
+
+    public ReqRes refreshToken(ReqRes refreshTokenReqiest){
+        ReqRes response = new ReqRes();
+        String ourEmail = jwtUtils.extractUsername(refreshTokenReqiest.getToken());
+        User users = ourUserRepo.findByEmail(ourEmail).orElseThrow();
+        if (jwtUtils.isTokenValid(refreshTokenReqiest.getToken(), users)) {
+            var jwt = jwtUtils.generateToken(users);
+            response.setStatusCode(200);
+            response.setToken(jwt);
+            response.setRefreshToken(refreshTokenReqiest.getToken());
+            response.setExpirationTime("24Hr");
+            response.setMessage("Successfully Refreshed Token");
+        }
+        response.setStatusCode(500);
+        return response;
+    }
+}
