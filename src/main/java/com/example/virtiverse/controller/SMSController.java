8145ff92c775769b36b26bcca27194351034cac6
fedibenameur11 <fedi.benameur@esprit.tn>
2024-04-20 20:05:49 +0100

    SMS-api

--- src/main/java/com/example/virtiverse/controller/SMSController.java
+++ src/main/java/com/example/virtiverse/controller/SMSController.java
@@ -1,2 +1,29 @@
-package com.example.virtiverse.controller;public class SMSController {
+package com.example.virtiverse.controller;
+
+import com.example.virtiverse.entities.SMSRequest;
+import com.example.virtiverse.serviceImp.SMSServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SMSController {
+
+    private final SMSServiceImpl smsService;
+
+    @Autowired
+    public SMSController(SMSServiceImpl smsService) {
+        this.smsService = smsService;
+    }
+
+    @PostMapping("/send-sms")
+    public String sendSMS(@RequestBody SMSRequest smsRequest) {
+        try {
+            smsService.sendSMS(smsRequest.getRecipient(), smsRequest.getMessage());
+            return "SMS sent successfully";
+        } catch (Exception e) {
+            return "Failed to send SMS: " + e.getMessage();
+        }
+    }
 }
--- src/main/java/com/example/virtiverse/entities/SMSRequest.java
+++ src/main/java/com/example/virtiverse/entities/SMSRequest.java
@@ -1,2 +1,23 @@
-package com.example.virtiverse.entities;public class SMSRequest {
+package com.example.virtiverse.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class SMSRequest {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id_SMS;
+    String recipient;
+    String message;
 }
